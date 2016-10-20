98b6ecd918906bb7385bcffd2e7702e332c45033
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2016-10-20 15:22:03 -0400

    Update EmployeeDbMapper.java

--- OpenClose/src/openClose/EmployeeDbMapper.java
+++ OpenClose/src/openClose/EmployeeDbMapper.java
@@ -3,6 +3,24 @@ package openClose;
 import java.lang.reflect.Constructor;
 
 public class EmployeeDbMapper {
+	private static final Map<int, Class<? extends Employee>> employeeTypes = new HashMap<>() {{
+		put(0, SalariedEmployee.class);
+		put(1, Intern.class);
+		put(2, Contractor.class);
+	}};
+
+	public static Employee create(int id, float hours, int type) {
+		try {
+			Constructor<? extends Employee> constructor = employeeTypes(type).getConstructor(int.class, float.class);
+			return constructor.newInstance(id, hours);
+		} catch (Exception e) { // wtf ......
+			e.printStackTrace();
+		}
+
+		return null; // wtf ......
+	}
+	
+	/*
 	public static Employee create(int id, float hours, int type) {
 		try {
 			Constructor<? extends Employee> constructor = EmployeeType.SALARIED.getConstructor();
@@ -30,5 +48,6 @@ public class EmployeeDbMapper {
 		public Constructor<? extends Employee> getConstructor() throws NoSuchMethodException, SecurityException {
 			return clazz.getConstructor(int.class, float.class);
 		}
-	}	
+	}
+	*/
 }
