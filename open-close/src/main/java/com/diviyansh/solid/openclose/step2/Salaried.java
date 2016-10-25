48da366cad4a0f3611cdf30af075bcb606a14939
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2016-10-25 14:28:10 -0400

    [open-close] [step-2] Salaried extends Employee

--- open-close/src/main/java/com/diviyansh/solid/openclose/step2/Salaried.java
+++ open-close/src/main/java/com/diviyansh/solid/openclose/step2/Salaried.java
@@ -1,22 +1,17 @@
 package com.diviyansh.solid.openclose.step2;
 
-import com.diviyansh.solid.openclose.step0.Employee;
-
-public class Salaried {
-
+public class Salaried extends Employee {
 	private static final int HOURLY_PAY = 1_000;
-	private Employee employee;
 
-	public Salaried(Employee employee) {
-		this.employee = employee;
+	public Salaried(int id, String type, float hours) {
+		super(id, type, hours);
 	}
 
 	public float weeklyPay() {
-		if (employee.getHours() < 40) {
-			return employee.getHours() * HOURLY_PAY;
+		if (getHours() < 40) {
+			return getHours() * HOURLY_PAY;
 		} else {
 			return 40 * HOURLY_PAY;
 		}
 	}
-
 }
