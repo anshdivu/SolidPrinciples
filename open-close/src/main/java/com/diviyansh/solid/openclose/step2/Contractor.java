4c383776a5f3e9508bc4a69f4a356d7ee985fd0e
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2016-10-25 14:25:01 -0400

    [open-close] [step-2] Contractor extends Employee

--- open-close/src/main/java/com/diviyansh/solid/openclose/step2/Contractor.java
+++ open-close/src/main/java/com/diviyansh/solid/openclose/step2/Contractor.java
@@ -1,18 +1,14 @@
 package com.diviyansh.solid.openclose.step2;
 
-import com.diviyansh.solid.openclose.step0.Employee;
-
-public class Contractor {
+public class Contractor extends Employee {
 	private static final int HOURLY_PAY = 1_200;
-	private Employee employee;
 
-	public Contractor(Employee employee) {
-		this.employee = employee;
+	public Contractor(int id, String type, float hours) {
+		super(id, type, hours);
 	}
 
-
 	public float weeklyPay() {
-		float pay = employee.getHours() * HOURLY_PAY;
+		float pay = getHours() * HOURLY_PAY;
 
 		if (pay < 30_000) {
 			return 30_000;
@@ -20,5 +16,4 @@ public class Contractor {
 			return pay;
 		}
 	}
-
 }
