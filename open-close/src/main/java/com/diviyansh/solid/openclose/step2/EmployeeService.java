ec22ab7d6860f95d0360bb48ec641659a915a023
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2016-10-25 14:16:29 -0400

    [open-close] [step-2] instanceOf checks in service

--- open-close/src/main/java/com/diviyansh/solid/openclose/step2/EmployeeService.java
+++ open-close/src/main/java/com/diviyansh/solid/openclose/step2/EmployeeService.java
@@ -1,19 +1,20 @@
 package com.diviyansh.solid.openclose.step2;
 
-import com.diviyansh.solid.openclose.step0.Employee;
-
 public class EmployeeService {
 
-	public float weekelyPay(Employee employee) {
-		switch (employee.getType()) {
-		case INTERN:
-			return new Intern(employee).weeklyPay();
-		case SALARIED:
-			return new Salaried(employee).weeklyPay();
-		case CONTRACTOR:
-			return new Contractor(employee).weeklyPay();
-		default:
-			return 0;
+	public float weeklyPay(Employee employee) {
+		if (employee instanceof Intern) {
+			return ((Intern) employee).weeklyPay();
 		}
+
+		if (employee instanceof Contractor) {
+			return ((Contractor) employee).weeklyPay();
+		}
+
+		if (employee instanceof Salaried) {
+			return ((Salaried) employee).weeklyPay();
+		}
+
+		return 0;
 	}
 }
