7b2053ecdf8ebdd85578ae5c5bcb62da91e783d7
Diviyansh Bhatnagar <ansh.divu@gmail.com>
2016-10-31 14:18:46 -0400

    [open-close][step-4] added getId/getHours methods to interface

--- open-close/src/main/java/com/diviyansh/solid/openclose/step4/Employee.java
+++ open-close/src/main/java/com/diviyansh/solid/openclose/step4/Employee.java
@@ -1,6 +1,8 @@
 package com.diviyansh.solid.openclose.step4;
 
 public interface Employee {
+	int getId();
+	float getHours();
 	float weeklyPay();
 	
 	public static Employee create(String type, int id, float hours) {
